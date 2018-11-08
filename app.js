588fbb13b2c976a0b6ea1ada7e8bd30e1a0f0019
Celia Marshall <celia.m@gmail.com>
2018-11-08 12:58:49 -0800

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
